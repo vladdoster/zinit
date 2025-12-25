af18f46f23f998185113b05b48bf71ff1e38b4c3
vladislav doster <mvdoster@gmail.com>
2025-12-25 14:43:35 -0600

    Refactor build system detection logic in script

--- share/gh-clone-build.zsh
+++ share/gh-clone-build.zsh
@@ -158,10 +158,10 @@ gh-clone-build() {
     if [[ -f CMakeLists.txt ]]; then
         build_system="cmake"
         (( verbose )) && print "Detected CMake build system"
-    elif [[ -f configure.ac || -f configure.in || -f configure ]] && [[ ! -f *[Mm]akefile ]] ); then
+    elif [[ ( -f configure.(in|ac) || -f configure ) && ! -f *[Mm]akefile ]]; then
         build_system="autotools"
         (( verbose )) && print "Detected Autotools build system"
-    elif ( [[ -f Makefile || -f makefile || -f GNUmakefile ]]; then
+    elif [[ -f [Mm]akefile || -f GNUmakefile ]]; then
         build_system="make"
         (( verbose )) && print "Detected Make build system"
     else
